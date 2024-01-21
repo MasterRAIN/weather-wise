f44f83341cb5736a34631b9f51a3666127e221da
Rainier Barbacena <rainierbarbacena@gmail.com>
2024-01-21 15:23:45 +0800

    finished the design

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -28,24 +28,60 @@ const Weather = () => {
       {weatherData ? (
         <>
           <div className="relative h-screen w-screen text-white">
-            <img className="absolute top-0 z-0 h-screen w-screen object-cover" src={process.env.PUBLIC_URL + "/assets/images/weather-bg3.jpg"} alt="background-image" />
+            <img className="absolute top-0 z-0 h-screen w-screen object-cover" src={process.env.PUBLIC_URL + "/assets/images/weather-bg.webp"} alt="background-image" />
             <div className="absolute z-10 h-screen w-screen"></div>
             <div className="absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
               <div className="h-form w-form rounded-3xl p-16 bg-blue-900 backdrop-blur-sm bg-opacity-50 shadow-md">
+                <div className="border flex mb-4">
+                  <input className="h-8 w-full bg-transparent border-0 outline-none px-4 focus:ring-2" type="text" />
+                  <button className="h-8 px-4 bg-white text-blue-900 hover:bg-blue-100">Search</button>
+                </div>
                 <div className="h-3/5 grid-cols-2 flex">
-                  <div className="w-3/5 p-4">
+                  <div className="w-3/5 py-4">
                     <h1 className="text-4xl">{weatherData.name}, {weatherData.sys.country}</h1>
-                    <h1 className="text-lg">January 21, 2024</h1>
+                    <h1 className="text-lg">January 21 2024, Sunday</h1>
                     <h1 className="text-sm">09:52:23 AM</h1>
                     <br />
                     <h1 className="text-7xl">27°C</h1>
                   </div>
                   <div className="w-2/5 flex flex-col items-center">
-                    <img className="h-48 w-48 -mt-4" src={process.env.PUBLIC_URL + "/assets/images/clouds.png"} alt="clouds" />
-                    <h1 className="text-3xl">Cloudy</h1>
+                    <img className="h-48 w-48 -mt-2" src={process.env.PUBLIC_URL + "/assets/images/cloudy.png"} alt="clouds" />
+                    <h1 className="text-3xl -mt-2">Cloudy</h1>
                   </div>
                 </div>
-                <div className="h-2/5"></div>
+                <div className="h-1/4">
+                  <div>Details</div>
+                  <div className="h-full flex items-center border-t">
+                    <div className="w-1/4 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/pressure.png"} alt="" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Pressure</h1>
+                        <h1>1014 hpa</h1>
+                      </div>
+                    </div>
+                    <div className="w-1/4 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/wind.png"} alt="" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Wind</h1>
+                        <h1>9 km/h</h1>
+                      </div>
+                    </div>
+                    <div className="w-1/4 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/humidity.png"} alt="" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Humidity</h1>
+                        <h1>74 %</h1>
+                      </div>
+                    </div>
+                    <div className="w-1/4 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-12" src={process.env.PUBLIC_URL + "/assets/images/temperature.png"} alt="" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Feels like</h1>
+                        <h1>30° C</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               {/* <img className="h-screen w-screen" src={process.env.PUBLIC_URL + "/assets/images/weather-bg3.jpg"} alt="" /> */}
                 {/* <h1>{weatherData.name}, {weatherData.sys.country}</h1>
                 <div>
