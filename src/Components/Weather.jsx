13744a0f8e2aea4b72a46ccf0e5c637f4d6856a5
Rainier Barbacena <rainierbarbacena@gmail.com>
2024-01-21 11:06:23 +0800

    50% on the Design

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -28,11 +28,24 @@ const Weather = () => {
       {weatherData ? (
         <>
           <div className="relative h-screen w-screen text-white">
-            <img className="absolute top-0 z-0 h-screen w-screen object-cover" src={process.env.PUBLIC_URL + "/assets/images/weather-bg3.jpg"} alt="" />
+            <img className="absolute top-0 z-0 h-screen w-screen object-cover" src={process.env.PUBLIC_URL + "/assets/images/weather-bg3.jpg"} alt="background-image" />
             <div className="absolute z-10 h-screen w-screen"></div>
             <div className="absolute z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-              <div className="h-form w-form rounded-3xl bg-blue-300 backdrop-blur-sm shadow bg-opacity-25">
-                
+              <div className="h-form w-form rounded-3xl p-16 bg-blue-900 backdrop-blur-sm bg-opacity-50 shadow-md">
+                <div className="h-3/5 grid-cols-2 flex">
+                  <div className="w-3/5 p-4">
+                    <h1 className="text-4xl">{weatherData.name}, {weatherData.sys.country}</h1>
+                    <h1 className="text-lg">January 21, 2024</h1>
+                    <h1 className="text-sm">09:52:23 AM</h1>
+                    <br />
+                    <h1 className="text-7xl">27°C</h1>
+                  </div>
+                  <div className="w-2/5 flex flex-col items-center">
+                    <img className="h-48 w-48 -mt-4" src={process.env.PUBLIC_URL + "/assets/images/clouds.png"} alt="clouds" />
+                    <h1 className="text-3xl">Cloudy</h1>
+                  </div>
+                </div>
+                <div className="h-2/5"></div>
               {/* <img className="h-screen w-screen" src={process.env.PUBLIC_URL + "/assets/images/weather-bg3.jpg"} alt="" /> */}
                 {/* <h1>{weatherData.name}, {weatherData.sys.country}</h1>
                 <div>
