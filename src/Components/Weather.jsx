943a03345d46d84f45f093e00ee9fc7cdc995f0f
Rainier Barbacena <rainierbarbacena@gmail.com>
2024-01-22 15:32:49 +0800

    added loading

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -83,12 +83,12 @@ const Weather = () => {
 
   return (
     <div className="weather-app">
-      {weatherData ? (
         <div className="relative h-screen w-screen text-white">
           {/* Background Image */}
           <img className="absolute top-0 z-0 h-screen w-screen object-cover" src={process.env.PUBLIC_URL + "/assets/images/weather-bg.webp"} alt="background-image" />
           
           {/* Main Content */}
+          {weatherData ? (
           <div className="absolute z-10 h-screen w-screen">
             <div className="absolute z-40 sm:top-1/2 sm:-translate-y-1/2 left-1/2 -translate-x-1/2">
               {/* Search Bar */}
@@ -174,10 +174,91 @@ const Weather = () => {
               <h1 className="text-center text-sm text-white">Developed by <a className="underline" href="https://www.linkedin.com/in/rainier-barbacena">Rainier Conde Barbacena</a></h1>
             </div>
           </div>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+        ) : (
+          <div className="absolute z-10 h-screen w-screen">
+            <div className="absolute z-40 sm:top-1/2 sm:-translate-y-1/2 left-1/2 -translate-x-1/2">
+              {/* Search Bar */}
+              <div className="sm:h-form xs:h-screen sm:w-form xs:w-screen sm:rounded-3xl sm:p-16 xs:py-10 xs:px-5 bg-blue-900 backdrop-blur-sm bg-opacity-50 shadow-md">
+                <div className="border flex mb-4">
+                  <input
+                    className="h-8 w-full bg-transparent border-0 outline-none px-4 focus:ring-2"
+                    type="text"
+                    placeholder="Enter a country"
+                  />
+                  <button className="h-8 px-4 bg-white text-blue-900 hover:bg-blue-100">
+                    Search
+                  </button>
+                </div>
+
+                {/* Weather Details */}
+                <div className="sm:h-3/5 grid-cols-2 sm:flex">
+                  <div className="sm:w-3/5 py-4">
+                    <h1 className="sm:text-4xl xs:text-2xl font-bold">Loading...</h1>
+                    <h1 className="text-lg">loading...</h1>
+                    <h1 className="text-sm">loading...</h1>
+                    <br className="sm:block xs:hidden" />
+                    <h1 className="text-7xl sm:block xs:hidden">... °C</h1>
+                  </div>
+
+                  {/* Weather Icon and Description */}
+                  <div className="sm:w-2/5 flex flex-col items-center">
+                    <img className="h-48 w-48 -mt-2" alt="weather-icons" />
+                    <h1 className="text-3xl -mt-2 font-bold">...</h1>
+                    <h1 className="text-2xl sm:hidden xs:block">... °C</h1>
+                  </div>
+                </div>
+
+                {/* Additional Weather Details */}
+                <div className="sm:h-1/4 xs:pt-4">
+                  <div>Details</div>
+                  <div className="h-full sm:flex xs:grid grid-cols-2 sm:gap-0 xs:gap-4 xs:pt-4 items-center border-t">
+                    {/* Pressure */}
+                    <div className="sm:w-1/4 sm:w-1/2 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/pressure.png"} alt="pressure" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Pressure</h1>
+                        <h1>... hpa</h1>
+                      </div>
+                    </div>
+
+                    {/* Wind */}
+                    <div className="sm:w-1/4 sm:w-1/2 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/wind.png"} alt="wind speed" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Wind</h1>
+                        <h1>... km/h</h1>
+                      </div>
+                    </div>
+
+                    {/* Humidity */}
+                    <div className="sm:w-1/4 sm:w-1/2 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-fit" src={process.env.PUBLIC_URL + "/assets/images/humidity.png"} alt="humidity" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Humidity</h1>
+                        <h1>... %</h1>
+                      </div>
+                    </div>
+
+                    {/* Feels Like */}
+                    <div className="sm:w-1/4 sm:w-1/2 flex items-center gap-4">
+                      <div className="h-12 w-12 grid place-content-center"><img className="h-12" src={process.env.PUBLIC_URL + "/assets/images/temperature.png"} alt="temperature" /></div>
+                      <div className="text-sm font-bold">
+                        <h1>Feels like</h1>
+                        <h1>... °C</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Bottom Credit */}
+            <div className="bottom-10 absolute z-50 w-full">
+              <h1 className="text-center text-sm text-white">Developed by <a className="underline" href="https://www.linkedin.com/in/rainier-barbacena">Rainier Conde Barbacena</a></h1>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
